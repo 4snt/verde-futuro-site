17d74f109236144c9be60f818f4eb7294905973f
4snt <murilo.escobedo@protonmail.com>
2024-12-02 22:18:16 -0300

    murilo

--- src/pages/Terms/pt-br.ts
+++ src/pages/Terms/pt-br.ts
@@ -160,7 +160,7 @@ function getPtHtml() {
       <h2>Contate-nos</h2>
       <p>Se você tiver alguma dúvida sobre esta Política de Privacidade, você pode nos contatar:</p>
       <ul>
-      <li>Visitando esta página em nosso site: <a href="verdefuturo.com/terms" rel="external nofollow noopener" target="_blank">verdefuturo.com/terms</a></li>
+      <li>Visitando esta página em nosso site: <a href="verdefuturo.com/terms" rel="external nofollow noopener" target="_blank">projetoverdefuturo.com.br/terms</a></li>
       </ul>
     `;
 
